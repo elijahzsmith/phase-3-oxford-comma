5ef3792ccc0839457844bea2b8fba67a932007cf
Elijah Smith <elijahsmith.m@gmail.com>
2022-04-24 22:56:25 -0500

    commit tree 64805102af0be25bceee73da089964497b7f53bd

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+    if array.length == 1
+        array.join("")
+    elsif array.length == 2
+        array.join(" and ")
+    elsif array.length == 3
+        array.slice(0, 2).join(", ") + ", and " + array.slice(2).to_s
+    else
+        array.slice(0, array.length-1).join(", ") + ", and " + array.slice(array.length-1).to_s
+    end
 end
